442880259435b74da3b5f94054526ea05e9cf1a5
Kinza <kinzashaikh38@gmail.com>
2022-06-19 01:31:07 +0500

    updated

--- ReligiousAssistant/App.js
+++ ReligiousAssistant/App.js
@@ -12,13 +12,6 @@ import useColorScheme from 'react-native/Libraries/Utilities/useColorScheme';
 import RootNavigator from './src/navigation/RootNavigator';
 import store from './src/redux/store'
 import {Provider} from 'react-redux'
-import Profile from './src/screens/muslim_module/Profile';
-import Settings from './src/screens/muslim_module/Settings';
-import AddMosque from './src/screens/muslim_module/AddMosque';
-import AnnouncementNoti from './src/screens/muslim_module/AnnouncementNoti';
-import ImamConsensusNoti from './src/screens/muslim_module/ImamConsensusNoti';
-import ApplyAsImam from './src/screens/muslim_module/ApplyAsImam'
-import MosqueConsensusNoti from './src/screens/muslim_module/MosqueConsensusNoti';
 const preloadedState = window.__PRELOADED_STATE__
 
 
@@ -29,7 +22,7 @@ const App = () => {
     <Provider store={store} serverState={preloadedState}>
     <NativeBaseProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-     <ImamConsensusNoti/>
+     <RootNavigator/>
     </NativeBaseProvider>
     </Provider>
   );
