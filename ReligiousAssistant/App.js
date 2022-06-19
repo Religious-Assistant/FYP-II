e7b29987031c80dfe3ee1775fb39a13ce9b11e3a
Kinza <kinzashaikh38@gmail.com>
2022-06-19 15:18:56 +0500

    Hindu Announcements

--- ReligiousAssistant/App.js
+++ ReligiousAssistant/App.js
@@ -12,7 +12,7 @@ import useColorScheme from 'react-native/Libraries/Utilities/useColorScheme';
 import RootNavigator from './src/navigation/RootNavigator';
 import store from './src/redux/store';
 import {Provider} from 'react-redux';
-import AddTemple from './src/screens/hindu_module/AddTemple';
+import Announcements from './src/screens/hindu_module/Announcements';
 
 const preloadedState = window.__PRELOADED_STATE__;
 
@@ -23,7 +23,7 @@ const App = () => {
     <Provider store={store} serverState={preloadedState}>
       <NativeBaseProvider>
         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <AddTemple />
+        <Announcements/>
       </NativeBaseProvider>
     </Provider>
   );
