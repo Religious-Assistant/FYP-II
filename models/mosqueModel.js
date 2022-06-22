56d7e667d00e1f244dca2273691fb2a72e11d2f7
nadirhussainnn <nadirhussaintumrani@gmail.com>
2022-06-22 10:01:53 +0500

    mosque add, finded etc

--- models/mosqueModel.js
+++ models/mosqueModel.js
@@ -2,6 +2,26 @@ const mongoose=require('mongoose')
 
 const mosqueSchema=mongoose.Schema({
     
+    mosqueName:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:{
+            type:String, 
+        },
+        coordinates:[]
+    },
+    addedBy:{
+        type:String,
+        required:true
+    },
+    verified:{
+        type:Boolean,
+        default:false,
+    }
 })
 
+mosqueSchema.index({location:'2dsphere'})
+ 
 module.exports=mongoose.model('Mosque', mosqueSchema)
--- routes/mosqueRoute.js
+++ routes/mosqueRoute.js
@@ -5,7 +5,9 @@ const mosque_controller=require('../controllers/mosqueController')
 const authMiddleWare=require('../middlewares/authMiddleWare')
 
 
-mosque_route.post('/addMosque',mosque_controller.addMosque)
-mosque_route.get('/getClosestMosques',mosque_controller.getClosestMosques)
+mosque_route.get('/getAllMosques',mosque_controller.getAllMosques)
+mosque_route.post('/getClosestMosques',mosque_controller.getClosestMosques)
+mosque_route.post('/getUnverifiedMosquesAroundUser',authMiddleWare,mosque_controller.getUnverifiedMosquesAroundUser)
+mosque_route.post('/addMosque',authMiddleWare,mosque_controller.addMosque)
 
 module.exports=mosque_route
