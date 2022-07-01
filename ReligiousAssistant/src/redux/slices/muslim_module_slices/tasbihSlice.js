d1b4aca9ad17b6212d931ae5ceabbacb0caf9019
Kinza <kinzashaikh38@gmail.com>
2022-07-01 16:46:54 +0500

    Created tasbihSlice

---
+++ ReligiousAssistant/src/redux/slices/muslim_module_slices/tasbihSlice.js
@@ -0,0 +1,47 @@
+import {createSlice,createAsyncThunk} from '@reduxjs/toolkit'
+import {apiPATCH} from '../../../services/apis/AuthService'
+import {update_tasbih} from '../../endpoints';
+
+
+const initialState = {
+    tasbihCounter:null,
+    isLoading:true,
+    hasError:false,
+}
+
+export const updateTasbih = createAsyncThunk(
+    'updateTasbih',
+    async(body)=>{
+        const result =  await apiPATCH(update_tasbih,body)
+        return result
+    }
+)
+
+const tasbihSlice = createSlice({
+    name:"tasbih",
+    initialState,
+    reducers:{},
+    extraReducers:{
+        [updateTasbih.fulfilled]:(state,action)=>{
+            state.hasError = false
+            state.isLoading = false
+        },
+        [updateTasbih.pending]:(state,action)=>{
+            state.isLoading=true
+            state.hasError=false
+        },
+        [updateTasbih.rejected]:(state,action)=>{
+            state.hasError = true
+            state.isLoading = false
+        },
+
+    }
+})
+
+
+export const selectAnnouncements =(state)=> state.tasbih.tasbihCounter
+export const selectIsLoading=(state)=>state.announcement.isLoading
+export const selectHasError=(state)=>state.announcement.hasError
+
+
+export default tasbihSlice.reducer
