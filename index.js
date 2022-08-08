93740b9be92b1eda99e1d96f43d1138a3f59e0bb
johncaden124 <holland.caden.04@gmail.com>
2022-08-08 18:09:09 +0500

    updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app=require('./app')
 const dotenv=require('dotenv')
 dotenv.config()
 
-const port=process.env.PORT
+const port=process.env.PORT || 8092
 
 app.listen(port,()=>{
     console.log(`Server listening at port ${port}`)
