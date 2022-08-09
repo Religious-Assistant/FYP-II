e1a25141c062951ce938d167ec34af2dc3061c0d
johncaden124 <holland.caden.04@gmail.com>
2022-08-09 16:28:35 +0500

    updated

--- routes/common_routes/userRoute.js
+++ routes/common_routes/userRoute.js
@@ -8,7 +8,7 @@ user_route.post('/register-user',registerUser)
 user_route.post('/login-user',loginUser)
 user_route.patch('/update-password',authMiddleWare,updatePassword)
 user_route.patch('/forgot-password',forgotPassword)
-user_route.post('/update-profile-image',authMiddleWare,updateProfileImage)
+user_route.post('/update-profile-image',updateProfileImage) //authMiddleWare
 user_route.get('/get-OTP-code',sendOTPCode)
 user_route.post('/verify-otp-code',verifyOTPCode)
 user_route.get('/delete-user',deleteUser)
