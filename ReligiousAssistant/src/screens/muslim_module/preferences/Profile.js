b16c20c210bb065c7eaeed0afab817e72949d3c1
Kinza <kinzashaikh38@gmail.com>
2022-08-12 22:42:04 +0500

    Updated user profile

--- ReligiousAssistant/src/screens/muslim_module/preferences/Profile.js
+++ ReligiousAssistant/src/screens/muslim_module/preferences/Profile.js
@@ -3,7 +3,7 @@
  * @version 1.0
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View, Image} from 'react-native';
 import {VStack, HStack, Text, Divider, Icon, ScrollView} from 'native-base';
 import CustomButton from '../../../components/CustomButton';
@@ -17,38 +17,74 @@ import EvilIcons from 'react-native-vector-icons/EvilIcons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {selectUserData} from '../../../redux/slices/auth_slices/authSlice';
+import {useSelector} from 'react-redux';
+import Geocoder from 'react-native-geocoding';
 
+Geocoder.init('AIzaSyAYgN_qJ-teJ5AJxO05TWaH35gcs5StQNE');
 export default function Profile() {
+  const [location, setLocation] = useState(null);
+  const user = useSelector(selectUserData);
+  const [userData, setUserData] = useState();
+
+  useEffect(() => {
+    if (user) {
+      setUserData(user);
+    }
+    {
+      user
+        ? Geocoder.from(
+            user.location?.coordinates[1],
+            user.location?.coordinates[0],
+          )
+            .then(json => {
+              var addressComponent = json.results[0].address_components;
+              //setReg({address: addressComponent});
+              setLocation(addressComponent[1].long_name);
+            })
+
+            .catch(error => console.warn(error))
+        : '';
+    }
+  }, []);
 
   const userInfo = [
-    {
-      id: 1,
-      label: 'User Name',
-      info: 'Kinza Shaikh',
-      icon: <EvilIcons name="user" />,
-      iconSize: '8',
-    },
-    {
-      id: 2,
-      label: 'Password',
-      info: 'Kinza123',
-      icon: <EvilIcons name="eye" />,
-      iconSize: '8',
-    },
-    {
-      id: 3,
-      label: 'Phone Number',
-      info: '0335339221',
-      icon: <AntDesign name="phone" />,
-      iconSize: '6',
-    },
-    {
-      id: 4,
-      label: 'Location',
-      info: 'Sukkur IBA',
-      icon: <Ionicons name="location-outline" />,
-      iconSize: '6',
-    },
+    userData
+      ? {
+          id: 1,
+          label: 'User Name',
+          info: userData.username,
+          icon: <EvilIcons name="user" />,
+          iconSize: '8',
+        }
+      : undefined,
+    userData
+      ? {
+          id: 2,
+          label: 'Password',
+          info: 'Password is hashed',
+          icon: <EvilIcons name="eye" />,
+          iconSize: '8',
+        }
+      : undefined,
+    userData
+      ? {
+          id: 3,
+          label: 'Phone Number',
+          info: userData.mobile,
+          icon: <AntDesign name="phone" />,
+          iconSize: '6',
+        }
+      : undefined,
+    userData
+      ? {
+          id: 4,
+          label: 'Location',
+          info: location ? location : 'Location not set',
+          icon: <Ionicons name="location-outline" />,
+          iconSize: '6',
+        }
+      : undefined,
     {
       id: 5,
       label: 'Learn Namaz',
@@ -56,18 +92,21 @@ export default function Profile() {
       icon: <Ionicons name="game-controller-outline" />,
       iconSize: '6',
     },
-    {
-      id: 6,
-      label: 'Primary Mosque',
-      info: 'Sukkur IBA',
-      icon: <MaterialCommunityIcons name="mosque" />,
-      iconSize: '6',
-    },
+    userData
+      ? {
+          id: 6,
+          label: 'Primary Mosque',
+          info: userData.primaryMosque ? userData.primaryMosque : 'None',
+          icon: <MaterialCommunityIcons name="mosque" />,
+          iconSize: '6',
+        }
+      : undefined,
   ];
-  return (
+  console.log(user);
+  return user && userData ? (
     <View style={styles.container}>
       <View style={styles.header}></View>
-      <Image style={styles.avatar} source={avatar} />
+      <Image style={styles.avatar} source={{uri: user?.avatar}} />
       <ScrollView
         keyboardShouldPersistTaps="handled"
         flex={1}
@@ -92,7 +131,7 @@ export default function Profile() {
                       flexDirection: 'row',
                       justifyContent: 'space-between',
                     }}>
-                      {/* Icon */}
+                    {/* Icon */}
                     <Icon
                       as={user.icon}
                       size={user.iconSize}
@@ -113,31 +152,14 @@ export default function Profile() {
                 style={{
                   flexDirection: 'row',
                   justifyContent: 'space-between',
-                }}>
-                <Icon
-                  as={<AntDesign name="user" />}
-                  size={6}
-                  ml="2%"
-                  mt="-1"
-                  color={colors.primary}
-                />
-                <Text style={styles.label}>Apply As Imam:</Text>
-              </View>
-              {/* button to apply as Imaam */}
-              <CustomButton
-                title="Apply"
-                variant="solid"
-                color="white"
-                base="40%"
-                onPress={() => {
-                  console.log('Apply As Imam');
-                }}
-              />
+                }}></View>
             </HStack>
           </VStack>
         </View>
       </ScrollView>
     </View>
+  ) : (
+    <></>
   );
 }
 
@@ -168,13 +190,13 @@ const styles = StyleSheet.create({
   },
   label: {
     fontFamily: fonts.Signika.regular,
-    fontSize: 20,
+    fontSize: 19,
     padding: 5,
     color: colors.primary,
   },
   info: {
     fontFamily: fonts.Signika.regular,
-    fontSize: 20,
+    fontSize: 18,
     padding: 5,
     color: colors.tertiary,
     flexWrap: 'wrap',
