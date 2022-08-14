0a85671ce2391209b09128aab0ce3c107ed28ef3
Kinza <kinzashaikh38@gmail.com>
2022-08-14 20:35:08 +0500

    Updated Add Temple

--- ReligiousAssistant/src/screens/hindu_module/AddTemple.js
+++ ReligiousAssistant/src/screens/hindu_module/AddTemple.js
@@ -20,7 +20,55 @@ import templeICon from '../../../assets/images/temple2_ic.png';
 import TextInput from '../../components/TextInput';
 import CustomButton from '../../components/CustomButton';
 
+// import {useNavigation} from '@react-navigation/native';
+
+// import {ADD_MOSQUE, GOOGLE_MAP} from '../../../navigation/constants';
+// import {useDispatch, useSelector} from 'react-redux';
+// import {selectUserData} from '../../../redux/slices/auth_slices/authSlice';
+// import {
+//   addMosque,
+//   selectIsLoadingAddNewMosque,
+//   selectNewAddedMosque,
+// } from '../../../redux/slices/muslim_module_slices/mosqueSlice';
+import Loader from '../common/Loader';
+
 export default function AddMosque() {
+  // const navigator = useNavigation();
+  // const dispatch = useDispatch();
+
+  // const userData = useSelector(selectUserData);
+  // const isLoadingAddNewMosque = useSelector(selectIsLoadingAddNewMosque);
+  // const newMosque = useSelector(selectNewAddedMosque);
+
+  // const [mosqueName, setMosquename] = useState('');
+
+  // function openMap() {
+  //   navigator.navigate(GOOGLE_MAP, {screen: ADD_MOSQUE});
+  // }
+
+  // function changeHandler(text) {
+  //   setMosquename(text);
+  // }
+
+  // function addNewMosque() {
+  //   if (route && mosqueName && userData) {
+  //     dispatch(
+  //       addMosque({
+  //         longitude: route.params.longitude,
+  //         latitude: route.params.latitude,
+  //         mosqueName,
+  //         addedBy: userData.username,
+  //       }),
+  //     );
+
+  //     if(newMosque){
+  //       console.log(newMosque)
+  //     }
+
+  //   } else {
+  //     alert('Location and Name required');
+  //   }
+  // }
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={{flex: 1, backgroundColor: colors.white}}>
@@ -55,35 +103,52 @@ export default function AddMosque() {
             </Heading>
           </View>
         </View>
-        <View style={{flex: 0.83}} width="95%">
-          <Center
-            width="88%"
-            space={2}
-            maxW="88%"
-            marginTop={'35%'}
-            marginLeft={'8%'}
-            marginBottom={'5%'}>
-            <FormControl mt={'12%'}>
+        {/* <View style={{flex: 0.83}} width="95%">
+          {!isLoadingAddNewMosque ? (
+            <Center
+              width="88%"
+              space={2}
+              maxW="88%"
+              marginTop={'35%'}
+              marginLeft={'8%'}
+              marginBottom={'5%'}>
               <TextInput
-                textTitle="Enter Temple Name"
+                textTitle="Enter Mosque Name"
                 mt="50%"
                 icon={<MaterialCommunityIcons name="mosque" />}
+                onChangeText={changeHandler}
               />
-              <TextInput
-                textTitle="Location"
-                mt="5%"
-                icon={<Entypo name="location" />}
-              />
+
               <CustomButton
-                title="Add Temple"
+                title="Select Location"
+                variant="solid"
+                mt="8%"
+                color={colors.white}
+                base="99%"
+                onPress={() => {
+                  openMap();
+                }}
+              />
+              {route?.params ? (
+                <Text mt={5} fontFamily={fonts.Signika.bold}>
+                  {`Longitude: ${route.params?.longitude}\n Latitude: ${route.params?.latitude}`}
+                </Text>
+              ) : (
+                <></>
+              )}
+              <CustomButton
+                title="Add Mosque"
                 variant="solid"
                 mt="8%"
                 color="white"
                 base="99%"
+                onPress={addNewMosque}
               />
-            </FormControl>
-          </Center>
-        </View>
+            </Center>
+          ) : (
+            <Loader msg="Adding New Mosque" />
+          )}
+        </View> */}
       </View>
     </TouchableWithoutFeedback>
   );
